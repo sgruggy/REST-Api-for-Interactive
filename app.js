02cb9ad8b3fa04515f8951492f6dde0f49d67e91
Robert Lei <ml4988@nyu.edu>
2018-04-19 06:30:23 -0400

    added cims.nyu.edu to allowed access

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Origin', 'https://i6.cims.nyu.edu');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
